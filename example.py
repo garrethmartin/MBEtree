98984618a8b8be5be1689542767b01f0f7fbbe7b
garrethmartin <g.martin4@herts.ac.uk>
2019-12-05 14:31:45 -0700

    correction to example

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ XX = np.load('example_data.npy')
 X = XX[:,0:3]
 w = XX[:,3]
 
-rho = MBE.modified_breiman_density(X, weights=w)
+rho = MBEtree.modified_breiman_density(X, weights=w)
 
 fig, ax = plt.subplots(figsize=(8,7));
 h = ax.scatter(X[:,0], X[:,1], s=4, c=np.log10(rho), cmap='jet');
