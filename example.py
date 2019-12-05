063022f5c1d6e94ac0dce5bec03e339e0f8de0e2
garrethmartin <g.martin4@herts.ac.uk>
2019-12-05 15:27:02 -0700

    changed example to 2-d example

--- example.py
+++ example.py
@@ -3,13 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 XX = np.load('example_data.npy')
-X = XX[:,0:3]
+X = XX[:,0:2]
 w = XX[:,3]
 
 rho = MBEtree.modified_breiman_density(X, weights=w)
 
 fig, ax = plt.subplots(figsize=(8,7));
-h = ax.scatter(X[:,0], X[:,1], s=4, c=np.log10(rho), cmap='jet');
+h = ax.scatter(X[:,0], X[:,1], c=np.log10(rho), s=np.log10(w/np.min(w)), cmap='jet');
 ax.set_xticks([])
 ax.set_yticks([])
 ax.axis('equal')
